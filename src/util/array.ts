3008a85350874db644fd38b9a4780a4fe173b33b
__metamethods <tabledmetamethods@gmail.com>
2024-06-26 14:37:51 -0700

    refactor: remove unnecessary jsdocs

--- src/util/array.ts
+++ src/util/array.ts
@@ -1,8 +1,3 @@
-/**
- * Calculates the sum of all numbers in an array.
- * @param array - The array of numbers.
- * @returns The sum of all numbers in the array.
- */
 export function sum(array: number[]): number {
   return array.reduce((a, b) => a + b, 0);
 }
--- src/util/try.ts
+++ src/util/try.ts
@@ -1,10 +1,3 @@
-/**
- * Try to run a function, and return a default value if it throws an error
- * @template T - The return type of the function
- * @param fn - A function to try
- * @param defaultValue - A default value to return if the function throws an error
- * @returns {T} The result of the function or the default value
- */
 export function tryOrDefault<T>(fn: () => T, defaultValue: T): T {
   try {
     return fn();
